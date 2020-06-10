6760c1a9f77708b2e03b6e4017d94e9aa6a74af8
lk <lk@bitkan.com>
2020-06-10 15:20:22 +0800

    Unsfae 类绕过9.0私有函数调用限制

---
+++ app/src/main/java/com/example/okhttptest/utils/ReflectWrapper.java
@@ -0,0 +1,171 @@
+package com.example.okhttptest.utils;
+
+import android.support.annotation.Keep;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+//https://fucknmb.com/2018/10/05/%E4%B8%80%E7%A7%8D%E7%BA%AFJava%E5%B1%82%E7%BB%95%E8%BF%87Android-P%E7%A7%81%E6%9C%89%E5%87%BD%E6%95%B0%E8%B0%83%E7%94%A8%E9%99%90%E5%88%B6%E7%9A%84%E6%96%B9%E5%BC%8F/
+
+/**
+ * 上帝反射类 一种纯 Java 层绕过 Android P 私有函数调用限制的方式
+ */
+@Keep
+public class ReflectWrapper {
+    @Keep
+    private Object classLoaderOffsetHelper;
+
+    static {
+        try {
+            Class<?> VersionClass = Class.forName("android.os.Build$VERSION");
+            Field sdkIntField = VersionClass.getDeclaredField("SDK_INT");
+            sdkIntField.setAccessible(true);
+            int sdkInt = sdkIntField.getInt(null);
+            if (sdkInt >= 28) {
+                Field classLoader = ReflectWrapper.class.getDeclaredField("classLoaderOffsetHelper");
+                long classLoaderOffset = getClassLoaderOffset(classLoader);
+
+                if (getObject(ReflectWrapper.class, classLoaderOffset) instanceof ClassLoader) {
+                    Object originalClassLoader = getAndSetObject(ReflectWrapper.class, classLoaderOffset);
+                    Log.e("ReflectWrapper", originalClassLoader.getClass().toString());
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static long getClassLoaderOffset(Field classLoader) throws IllegalAccessException {
+        try {
+            Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field f = unsafeClass.getDeclaredField("theUnsafe");
+            f.setAccessible(true);
+            final Object unsafe = f.get(null);
+            final Method allocateInstance = unsafeClass.getMethod("objectFieldOffset", Field.class);
+            return (long) allocateInstance.invoke(unsafe, classLoader);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+
+    public static Object getObject(Class<?> mClass, long offset) throws IllegalAccessException {
+        try {
+            Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field f = unsafeClass.getDeclaredField("theUnsafe");
+            f.setAccessible(true);
+            final Object unsafe = f.get(null);
+            final Method allocateInstance = unsafeClass.getMethod("getObject", Object.class, Long.TYPE);
+            return allocateInstance.invoke(unsafe, mClass, offset);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Object getAndSetObject(Class<?> mClass, long offset) throws IllegalAccessException {
+        try {
+            Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field f = unsafeClass.getDeclaredField("theUnsafe");
+            f.setAccessible(true);
+            final Object unsafe = f.get(null);
+            final Method allocateInstance = unsafeClass.getMethod("getAndSetObject", Object.class, Long.TYPE, Object.class);
+            return allocateInstance.invoke(unsafe, mClass, offset, null);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+
+    public static Field findField(Object instance, String name) throws NoSuchFieldException {
+        for (Class<?> clazz = instance.getClass(); clazz != null; clazz = clazz.getSuperclass()) {
+            try {
+                Field field = clazz.getDeclaredField(name);
+
+                if (!field.isAccessible()) {
+                    field.setAccessible(true);
+                }
+
+                return field;
+            } catch (NoSuchFieldException e) {
+                // ignore and search next
+            }
+        }
+
+        throw new NoSuchFieldException("Field " + name + " not found in " + instance.getClass());
+    }
+
+    public static Method findMethod(Object instance, String name, Class<?>... parameterTypes)
+            throws NoSuchMethodException {
+        for (Class<?> clazz = instance.getClass(); clazz != null; clazz = clazz.getSuperclass()) {
+            try {
+                Method method = clazz.getDeclaredMethod(name, parameterTypes);
+
+                if (!method.isAccessible()) {
+                    method.setAccessible(true);
+                }
+
+                return method;
+            } catch (NoSuchMethodException e) {
+                // ignore and search next
+            }
+        }
+
+        throw new NoSuchMethodException("Method "
+                + name
+                + " with parameters "
+                + Arrays.asList(parameterTypes)
+                + " not found in " + instance.getClass());
+
+    }
+    public static Method findMethod(Class<?> clazz, String name, Class<?>... parameterTypes)
+            throws NoSuchMethodException {
+        for (; clazz != null; clazz = clazz.getSuperclass()) {
+            try {
+                Method method = clazz.getDeclaredMethod(name, parameterTypes);
+
+                if (!method.isAccessible()) {
+                    method.setAccessible(true);
+                }
+
+                return method;
+            } catch (NoSuchMethodException e) {
+                // ignore and search next
+            }
+        }
+
+        throw new NoSuchMethodException("Method "
+                + name
+                + " with parameters "
+                + Arrays.asList(parameterTypes)
+                + " not found in " + clazz);
+    }
+}
