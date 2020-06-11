e785901e471389ae6c7e3709c7fe31e4cd265500
lk <lk@bitkan.com>
2020-06-11 17:57:57 +0800

    自定义classLoader

---
+++ app/src/main/java/com/example/okhttptest/myJava/ClassLoaderTest.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ClassLoaderTest {
+    public static void main(String[] args) {
+        DiskClassLoader diskClassLoader = new DiskClassLoader("/Users/lk/myJava");//1
+        try {
+            Class c = diskClassLoader.loadClass("com.example.Jobs1");//2
+            if (c != null) {
+                try {
+                    Object obj = c.newInstance();
+                    System.out.println(obj.getClass().getClassLoader());
+                    Method method = c.getDeclaredMethod("say", null);
+                    method.invoke(obj, null);//3
+                } catch (InstantiationException | IllegalAccessException
+                        | NoSuchMethodException
+                        | SecurityException |
+                        IllegalArgumentException |
+                        InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/okhttptest/myJava/Jobs1.java
@@ -0,0 +1,7 @@
+
+package com.example;
+public class Jobs1 {
+    public void say() {
+        System.out.println("One more thing");
+    }
+}
