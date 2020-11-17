8b8cf5240ec58c08d179f2894c3223e288a9d598
lk <lk@bitkan.com>
2020-11-17 14:09:39 +0800

    补充

--- app/src/main/java/com/example/okhttptest/myJava/proxy/ProxyTest.java
+++ app/src/main/java/com/example/okhttptest/myJava/proxy/ProxyTest.java
@@ -3,6 +3,7 @@ package com.example.okhttptest.myJava.proxy;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
+//https://www.jianshu.com/p/9bcac608c714
 public class ProxyTest {
     public static void main(String[] args) {
         System.getProperties().setProperty("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
