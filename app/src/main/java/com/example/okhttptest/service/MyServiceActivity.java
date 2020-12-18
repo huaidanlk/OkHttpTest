8553d732fb2021f6e00ef69e7b7d4cfa12321a57
lk <lk@bitkan.com>
2020-12-18 18:13:16 +0800

    lru

--- app/src/main/java/com/example/okhttptest/service/MyServiceActivity.java
+++ app/src/main/java/com/example/okhttptest/service/MyServiceActivity.java
@@ -7,7 +7,6 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.support.annotation.UiThread;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
