5f50d250a4746df163913fa95973c1fc6b4c6bf2
lk <lk@bitkan.com>
2021-01-15 10:53:48 +0800

    dp 排序

---
+++ app/src/main/java/com/example/okhttptest/leetcode/MergeSort.java
@@ -0,0 +1,53 @@
+package com.example.okhttptest.leetcode;
+
+public class MergeSort {
+    public static int[] arrays = new int[]{1, 5, 23, 3, 6, 2, 34, 63425, 24, 62, 568};
+    public static int[] leftArray = new int[arrays.length >> 1];
+
+    public static void main(String[] args) {
+
+
+        sort(0, arrays.length);
+
+
+        for(int i = 0;i<arrays.length;i++){
+            System.out.print(arrays[i] + "_");
+        }
+    }
+
+    public static void sort(int begin, int end) {
+        if (end - begin < 2) return;
+        int mid = (begin + end) >> 1;
+        sort(begin, mid);
+        sort(mid, end);
+        merge(begin, mid, end);
+    }
+
+    public static void merge(int begin, int mid, int end) {
+        int li = 0, le = mid - begin;
+
+        int ri = mid , re =end;
+        int ai = begin;
+
+        //复制左边的数组
+        for (int i = li; i < le; i++) {
+            leftArray[i] = arrays[begin + i];
+        }
+
+        // 左边还未结束
+        while (li < le){
+            // ri >=re  右边已经结束了， 把剩下的 leftArray 复制到 arrays
+            if( ri>=re || leftArray[li]<arrays[ri]){ //
+                arrays[ai] = leftArray[li];
+                li++;
+                ai++;
+            }else {
+                arrays[ai] = arrays[ri];
+                ri++;
+                ai++;
+            }
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/okhttptest/myJava/ClassLoaderTest.java
+++ app/src/main/java/com/example/okhttptest/myJava/ClassLoaderTest.java
@@ -29,4 +29,5 @@ public class ClassLoaderTest {
             e.printStackTrace();
         }
     }
+
 }
